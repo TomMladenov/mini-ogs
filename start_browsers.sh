120f4c287f6396599d97d22d8e4031bec9a1d618
Tom Mladenov <tom.mladenov@ieee.org>
2021-09-06 10:12:59 +0000

    Delete old files

--- start_browsers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-remote_host=192.168.0.240
-
-chromium-browser http://$remote_host:8000/docs#/ http://$remote_host:5000/ http://$remote_host:5000 http://$remote_host:3000
--- sync.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker cp . $(docker ps -aqf "name=ogs-core"):/opt/mini-ogs
